ac729fa128e353204e7216a184b95ffbe63bded4
Shuttle_11 <hemalatha11@gmail.com>
2018-08-29 21:23:56 +0000

    added package json and started app js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Added this line");
+var express = require("express");
+var app = express();
+
+
+
+app.get("/", function(req,res)
+{
+    res.send("Home Page");
+});
+
+app.listen(process.env.PORT,process.env.IP, function(req,res)
+{
+    console.log("Start your goals");
+});
