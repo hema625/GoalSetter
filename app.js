674e397bd10d86914250acfa017195dbaa6d40e3
Shuttle_11 <hemalatha11@gmail.com>
2018-08-31 02:02:37 +0000

    login register added

--- app.js
+++ app.js
@@ -2,11 +2,32 @@ var express = require("express");
 var app = express();
 
 
+//app config
+
+app.set("view engine", "ejs");
+
 
 app.get("/", function(req,res)
 {
-    res.send("Home Page");
+    //res.send("Home Page");
+    res.redirect("/login");
 });
+
+app.get("/login" , function(req,res)
+{
+    res.render("login");
+});
+
+app.get("/register" , function(req,res)
+{
+    res.render("register");
+});
+
+app.post("/viewpage", function(req,res)
+{
+   // res.send("In viewpage");
+   res.render("viewpage");
+})
 
 app.listen(process.env.PORT,process.env.IP, function(req,res)
 {
